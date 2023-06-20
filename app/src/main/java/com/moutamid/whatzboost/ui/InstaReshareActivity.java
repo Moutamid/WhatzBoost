f1116cffc2683074901a1831c0051b0e3b5fcf81
suleman81 <sunnybajwa904@gmail.com>
2023-06-20 18:36:25 +0500

    commit

--- app/src/main/java/com/moutamid/whatzboost/ui/InstaReshareActivity.java
+++ app/src/main/java/com/moutamid/whatzboost/ui/InstaReshareActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.moutamid.whatzboost.R;
+import com.moutamid.whatzboost.databinding.ActivityInstaReshareBinding;
 
 public class InstaReshareActivity extends AppCompatActivity {
-
+    ActivityInstaReshareBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_insta_reshare);
+        binding = ActivityInstaReshareBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backbtn.setOnClickListener(v -> onBackPressed());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
