66fcdb2dc3ca0f6ef4ff43e252e7745dbcf2883a
suleman81 <sunnybajwa904@gmail.com>
2023-07-11 16:12:59 +0500

    commit

--- app/src/main/java/com/moutamid/whatzboost/ui/WhatsWebActivity.java
+++ app/src/main/java/com/moutamid/whatzboost/ui/WhatsWebActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.webkit.DownloadListener;
 import android.webkit.WebChromeClient;
@@ -74,22 +76,23 @@ public class WhatsWebActivity extends AppCompatActivity implements AdvancedWebVi
             onBackPressed();
         });
 
-        String url = "https://web.whatsapp.com/";
+        String url = "https://web.whatsapp.com/%F0%9F%8C%90/en";
 
         binding.web.setListener(this, this);
         binding.web.setMixedContentAllowed(true);
-        enableSettingsOfWebView(binding.web);
         binding.web.setDesktopMode(true);
 //        setDesktopMode(binding.web, true);
 
+        enableSettingsOfWebView(binding.web);
         binding.web.loadUrl(url);
+
         binding.web.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int progress) {
                 super.onProgressChanged(view, progress);
 
                 if (progress == 100) {
-                    binding.progress.setVisibility(View.GONE);
+                    new Handler().postDelayed(() -> binding.progress.setVisibility(View.GONE), 4000);
                 }
             }
         });
@@ -99,10 +102,41 @@ public class WhatsWebActivity extends AppCompatActivity implements AdvancedWebVi
 
     @SuppressLint("SetJavaScriptEnabled")
     public static void enableSettingsOfWebView(AdvancedWebView webView) {
+        String agent = webView.getSettings().getUserAgentString();
+        Log.d("Hello", agent);
+        String newUserAgent = "";
+        try {
+            String androidOSString = webView.getSettings().getUserAgentString().substring(agent.indexOf("("), agent.indexOf(")") + 1);
+            newUserAgent = webView.getSettings().getUserAgentString().replace(androidOSString, "(X11; Linux x86_64)");
+        } catch (Exception e) {
+            e.printStackTrace();
+            newUserAgent = null;
+        }
+
+//        webView.getSettings().setUserAgentString(newUserAgent);
+//        String a = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"; // Chrome/57.0.2987.132
+        String a = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0";
+        webView.getSettings().setUserAgentString(a);
+        Log.d("Hello", a);
+        Log.d("Hello", "New User Agent : "+ newUserAgent);
         webView.getSettings().setBuiltInZoomControls(true);
-        webView.setThirdPartyCookiesEnabled(true);
-        webView.setCookiesEnabled(true);
+        webView.setThirdPartyCookiesEnabled(false);
+        webView.setCookiesEnabled(false);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setBlockNetworkImage(false);
+        webView.getSettings().setBlockNetworkLoads(false);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setDefaultTextEncodingName("UTF-8");
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.reload();
     }
 
     public void setDesktopMode(AdvancedWebView webview, boolean enabled) {
